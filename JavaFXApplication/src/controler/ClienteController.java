90c5c33f6aa2f7aa5eb69f092daef90e6be353d4
Frank_999 <franciscoestuardofranco2004@gmail.com>
2024-10-18 06:58:49 -0600

    Se comenzo con la parte grafica

--- JavaFXApplication/src/controler/ClienteController.java
+++ JavaFXApplication/src/controler/ClienteController.java
@@ -7,7 +7,12 @@ package controler;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -16,6 +21,27 @@ import javafx.fxml.Initializable;
  */
 public class ClienteController implements Initializable {
 
+    @FXML
+    private TextField TXT_Nombre;
+    @FXML
+    private TextField TXT_Edad;
+    @FXML
+    private TextField TXT_Correo;
+    @FXML
+    private TextField TXT_Telefono;
+    @FXML
+    private TableView<?> Mostrar_Todo;
+    @FXML
+    private TableColumn<?, ?> Colum_Id;
+    @FXML
+    private TableColumn<?, ?> Colum_Nombre;
+    @FXML
+    private TableColumn<?, ?> Colum_elefono;
+    @FXML
+    private TableColumn<?, ?> Colum_Correo;
+    @FXML
+    private TableColumn<?, ?> Colum_Edad;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +49,9 @@ public class ClienteController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void Abrir_Cliente(ActionEvent event) {
+    }
     
 }
--- JavaFXApplication/src/controler/MenuController.java
+++ JavaFXApplication/src/controler/MenuController.java
@@ -5,9 +5,15 @@
  */
 package controler;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -21,7 +27,60 @@ public class MenuController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
+    @FXML
+    private void Abrir_Cliente() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/cliente.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Libros");
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void Abrir_Libros() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/libro.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Libros");
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void Abrir_Prestamos() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/Prestamos.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = new Stage();
+            stage.setTitle("Prestamos");
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
